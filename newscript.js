3b618812850f7f229fe7a71b60275111595bbf90
Keith Fung <fungsenkeith@gmail.com>
2020-10-01 18:36:16 -0400

    IT WORKS!!

--- newscript.js
+++ newscript.js
@@ -12,8 +12,8 @@ function syncCharacterAmount(x) {
 }
 //
 //Range of generation
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
-const UPPER_CHAR_CODES = arrayFromLowToHigh(97, 122);
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58,64))
 const passwordDisplay = document.getElementById('passwordDisplay')
@@ -22,7 +22,7 @@ const form = document.getElementById('passwordGeneratorForm')
 function arrayFromLowToHigh(low, high){
     const array = []
     for (let i = low; i <= high; i++) {
-        array.push[i]
+        array.push(i)
     }
     return array
 }
@@ -38,11 +38,11 @@ const includeNumbersElement = document.getElementById("includeNumbers")
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    const characterAmount = characterAmountNumber.value
-    const includeUppercase = includeUppercaseElement.checked
-    const includeNumbers = includeNumbersElement.checked
-    const includeSymbols = includeSymbolsElement.checked
-    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+    const characterAmount = characterAmountNumber.value;
+    const includeUppercase = includeUppercaseElement.checked;
+    const includeNumbers = includeNumbersElement.checked;
+    const includeSymbols = includeSymbolsElement.checked;
+    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
     passwordDisplay.innerText = password
 })
 
@@ -50,14 +50,15 @@ form.addEventListener('submit', e => {
 function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUppercase){
     let charCodes = LOWERCASE_CHAR_CODES
     // if checked, it adds on the possible values of the list below.
-    if (includeUppercase) charCodes = charCodes.concat(UPPER_CHAR_CODES)
-    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-    const passwordCharacters = []
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
+    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
+
+    const passwordCharacters = [];
     for (let i= 0; i < characterAmount; i++) {
         // giving random characters to the amount of character codes 
-        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-        passwordCharacters.push(String.fromCharCode(characterCode))
+        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
+        passwordCharacters.push(String.fromCharCode(characterCode));
     }
     return passwordCharacters.join('')
 }
