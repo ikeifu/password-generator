3cc21413481d349290c5bfd35c8c32b4006bff0c
Keith Fung <fungsenkeith@gmail.com>
2020-10-01 19:39:54 -0400

    Weird Bug

--- newscript.js
+++ newscript.js
@@ -1,7 +1,7 @@
-// aligning characterAmountSlider and number value
+// linking variable and id together
 const characterAmountSlider = document.getElementById('characterAmountSlider');
 const characterAmountNumber = document.getElementById('characterAmountNumber');
-
+// aligning characterAmountSlider and number value
 characterAmountSlider.addEventListener('input', syncCharacterAmount)
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 
@@ -12,14 +12,14 @@ function syncCharacterAmount(x) {
 }
 //
 //Range of generation
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58,64))
-const passwordDisplay = document.getElementById('passwordDisplay')
-const form = document.getElementById('passwordGeneratorForm')
+const UPPERCASE_CHAR_CODES = randomNG(65, 90);
+// console.log("here " + UPPERCASE_CHAR_CODES);
+const LOWERCASE_CHAR_CODES = randomNG(97, 122);
+console.log("try " + LOWERCASE_CHAR_CODES);
+const NUMBER_CHAR_CODES = randomNG(48, 57);
+const SYMBOL_CHAR_CODES = randomNG(33, 47).concat(randomNG(58,64))
 
-function arrayFromLowToHigh(x, y){
+function randomNG(x, y){
     const array = []
     for (let i = x; i <= y; i++) {
         array.push(i)
@@ -34,6 +34,8 @@ const includeUppercaseElement = document.getElementById("includeUppercase")
 const includeSymbolsElement = document.getElementById("includeSymbols")
 // console.log(includeSymbols)
 const includeNumbersElement = document.getElementById("includeNumbers")
+const passwordDisplay = document.getElementById('passwordDisplay')
+const form = document.getElementById('passwordGeneratorForm')
 // console.log(includeNumbers)
 //
 
@@ -55,12 +57,12 @@ form.addEventListener('submit', e => {
 function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUppercase){
     let charCodes = LOWERCASE_CHAR_CODES
     // if checked, it adds on the possible values of the list below.
-    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-    console.log("first " + charCodes)
     if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-    console.log("second " + charCodes)
+    // console.log("first " + charCodes)
     if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-    console.log("third " + charCodes)
+    // console.log("second " + charCodes)
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    // console.log("third " + charCodes)
 
     const passwordCharacters = [];
     for (let i= 0; i < characterAmount; i++) {
